7be3cb676db371b9d9bd176d5f5f692efacdb95b
miospruit <mio.spruit@energyzero.nl>
2026-06-03 14:40:21 +0200

    Move hexagon status background behind square widget content

--- script_energy_square.js
+++ script_energy_square.js
@@ -223,6 +223,7 @@ async function main() {
 
   rect(ctx, 0, 0, W, H, C.bg);
   rect(ctx, 70, 70, W - 140, H - 140, C.card);
+  hexagon(ctx, W / 2 + 190, 610, 360, `${status.color}55`);
 
   text(ctx, "NU", 0, 120, W, 82, Font.heavySystemFont(72), C.orange, "center");
   text(
@@ -248,8 +249,6 @@ async function main() {
     C.text,
     "center",
   );
-
-  hexagon(ctx, W / 2 + 150, 610, 280, `${status.color}66`);
 
   text(
     ctx,
